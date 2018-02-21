fe3a0600c8eb2846e2778d2ad8d6488401b281af
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-02-21 23:22:54 +0530

    [ENH] expanded config, no cheating

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,13 +1,25 @@
-const extract = new (require("extract-text-webpack-plugin"))("../css/style.css")
+const Extract = require("extract-text-webpack-plugin")
+
+const extract = new Extract("../css/style.css")
+
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: process.cwd() + "/dist/js",
         filename: "bundle.js",
     },
-    module:{rules:[{
-        test: /\.scss$/,
-         use: extract.extract({ use: ["css-loader", "sass-loader"], fallback: "style-loader" })
-    }]},
-    plugins: [extract]
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: extract.extract({
+                    use: ["css-loader", "sass-loader"],
+                    fallback: "style-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        extract
+    ]
 }

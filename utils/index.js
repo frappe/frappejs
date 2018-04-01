d7363fb4a1667ff66319337626cb16bda50af8d0
Revant Nandgaonkar <revant.one@gmail.com>
2018-04-01 12:24:07 +0530

    getUid and getRandomInt added in frappe.utils

--- utils/index.js
+++ utils/index.js
@@ -66,4 +66,35 @@ module.exports = {
             });
     },
 
+    /**
+     * Return a unique identifier with the given `len`.
+     *
+     * @param {Number} length
+     * @return {String}
+     * @api private
+     */
+    getUid(length) {
+        let self = this;
+        let uid = '';
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charsLength = chars.length;
+
+        for (let i = 0; i < length; ++i) {
+          uid += chars[self.getRandomInt(0, charsLength - 1)];
+        }
+
+        return uid;
+    },
+
+    /**
+     * Return a random int, used by `utils.getUid()`.
+     *
+     * @param {Number} min
+     * @param {Number} max
+     * @return {Number}
+     * @api private
+     */
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 };

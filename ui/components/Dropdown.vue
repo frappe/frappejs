495898874df09863f3433c250b58765ec60fd277
Suraj Shetty <surajshetty3416@gmail.com>
2018-07-14 20:11:31 +0530

    Add dropdown component

--- ui/components/Dropdown.vue
+++ ui/components/Dropdown.vue
@@ -1,47 +1,19 @@
 <template>
   <div class="dropdown show">
-      <button
-        :id="_uid"
-        class="btn btn-sm btn-light dropdown-toggle"
-        aria-haspopup="true"
-        :aria-expanded="isShown ? 'true' : 'false'"
-        @click="isShown = !isShown"
-      >
-        {{ label }}
-      </button>
-      <div :class="['dropdown-menu dropdown-menu-right', isShown ? 'show' : '']" :aria-labelledby="_uid">
-        <a
-          href="#"
-          class="dropdown-item"
-          v-for="option in options"
-          :key="option.label"
-          @click.prevent="option.handler"
-        >
-          {{ option.label }}
-        </a>
+      <a class="btn btn-sm btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        {{label}}
+      </a>
+      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
+        <a class="dropdown-item" v-for="option in options" :key="option.label" @click="option.handler">{{option.label}}</a>
       </div>
   </div>
 </template>
 <script>
 export default {
-  props: ['label', 'options'],
-  data() {
-    return {
-      isShown: false
-    }
-  }
-};
-</script>
-<style lang="scss">
-@import "../styles/variables.scss";
-$dropdown-link-active-bg: $gray-300;
-
-.dropdown-item {
-  &.active,
-  &:active {
-    @include gradient-bg($dropdown-link-active-bg);
-  }
+  props: ['label', 'options']
 }
+</script>
+<style>
 
 </style>
 

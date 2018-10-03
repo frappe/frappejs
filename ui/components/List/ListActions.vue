1699e7f079fc24b9a3d5e8482ff163d30057d204
zamhaq <haque864@gmail.com>
2018-10-04 00:10:30 +0530

    Use props as object in ListActions to support defaultProps

--- ui/components/List/ListActions.vue
+++ ui/components/List/ListActions.vue
@@ -2,12 +2,20 @@
     <div class="frappe-list-actions d-flex justify-content-between align-items-center p-3 border-bottom">
         <h5 class="m-0">{{ doctype }} List</h5>
         <button v-if="showDelete" class="btn btn-danger btn-sm" @click="$emit('delete')">Delete</button>
-        <button v-else class="btn btn-primary btn-sm" @click="$emit('new')">New</button>
+        <button v-if="showNew" class="btn btn-primary btn-sm" @click="$emit('new')">New</button>
         <button v-if="showCreateKanban" class="btn btn-primary btn-sm" @click="$emit('createKanban')">Create Kanban</button>
     </div>
 </template>
 <script>
 export default {
-  props: ['doctype', 'showDelete', 'showCreateKanban']
+  props: {
+    doctype: String,
+    showDelete: Number,
+    showCreateKanban: Boolean,
+    showNew: {
+      type: Boolean,
+      default: true
+    }
+  }
 };
 </script>

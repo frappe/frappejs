37f576eb5b4de434ad67e2bb017989688542b859
anto-christo <anto.christo.20@gmail.com>
2018-10-14 17:12:37 +0530

    Add sample turn.config.js

---
+++ webrtc/turn.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    url: 'turn:xxx.xxx.x.xxxx:xxxx', //ip address and port number of stun-turn server
+    username: 'xxxxxx', //username for accessing stun-turn server
+    password: 'xxxxxx' //password for accessing stun-turn server
+}
